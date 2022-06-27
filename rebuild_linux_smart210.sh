d72fbd986f2825c7a1f9a015a4778c44a69b2d0b
Gonglja <glj0@outlook.com>
2022-06-27 20:13:42 +0800

    FEAT: 添加smart210板卡构建脚本

---
+++ rebuild_linux_smart210.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+## ubuntu 22.04 lts kernel 最新版本
+## 0.1 Install corss-compile-toochain
+# sudo apt install gcc-arm-none-eabi
+
+## 0.2 Dep
+# sudo apt install flex bison
+# sudo apt-get install libssl-dev
+# sudo apt install u-boot-tools 
+# sudo apt-get install libgmp-dev libmpc-dev
+
+## 0.3 issue
+# fdt error
+# sudo apt-get purge -y --auto-remove libfdt-dev
+
+# 0.4 配置编译链环境
+export ARCH=arm
+export CROSS_COMPILE=arm-none-eabi-
+
+OUT_DIR=/home/u/workspace/os/output
+
+# 0.5  清除以前编译
+make clean 
+make distclean
+make mrproper
+mkdir -p $OUT_DIR
+
+
+# 1.0 配置s5pv210默认配置，指定输出位置
+make s5pv210_fin_defconfig O=$OUT_DIR
+
+# 1.1 编译 uImage
+make LOADADDR=0x20004000 uImage O=$OUT_DIR -j$(nproc)
+
+# 1.2 编译 modules 并安装至./output下
+make modules O=$OUT_DIR -j$(nproc)
+make INSTALL_MOD_PATH=$OUT_DIR/res O=$OUT_DIR modules_install 
+
+# 1.3 编译 dtbs 并安装至./output下
+make dtbs  O=$OUT_DIR -j$(nproc)
+make INSTALL_DTBS_PATH=$OUT_DIR/res O=$OUT_DIR dtbs_install
+
+
+# echo "copy to ../rootfs/"
+# sudo cp -vrf $(OUT_DIR)/arch/arm/boot/uImage rootfs/
+# sudo cp -vrf $(OUT_DIR)/arch/arm/boot/dts/s5pv210-smart210.dtb rootfs/
